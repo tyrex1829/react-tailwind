a4014ada940f2ffc3945fad30d6964cb20b0481a
Saksham Khare <sakshamkhare19@gmail.com>
2024-04-30 10:06:21 +0530

    counter

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,23 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
+  const plusOne = () => {
+    setCount(count + 1);
+  }
+
+  const minusOne = () => {
+    if (count <= 0) {
+      alert("Value is already 0, Please add first to do minus operation");
+    } else {
+      setCount(count - 1);
+    }
+  }
+
   return (
     <>
-      <h1 className='bg-green-400 text-black rounded-xl p-4'>Tailwind with React(Vite)</h1>
+      <h1 className='mb-3.5'>Counter {count}</h1>
+      <button onClick={plusOne} className='mr-1'>+1</button>
+      <button onClick={minusOne} className='ml-1'>-1</button>
     </>
   )
 }
